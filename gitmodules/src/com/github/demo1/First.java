f2388d31b89217dac00cfe7d956540b5be997f17
秦风扬 <563467162@qq.com>
2021-04-25 15:31:06 +0800

    第一个ideagit项目

---
+++ gitmodules/src/com/github/demo1/First.java
@@ -0,0 +1,18 @@
+package com.github.demo1;
+
+/**
+ * @ClassName First
+ * @Description 案例
+ * @Author 夜航星
+ * @Date 2021\4\25 0025 15:16
+ * @Version 1.0
+ **/
+public class First {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+
+    }
+}
